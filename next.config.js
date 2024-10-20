d3deba5a12782014a8801afc6fa56503685b474d
dobe4ever2 <dacc862@gmail.com>
2024-10-20 10:01:22 +0000

    a

--- next.config.js
+++ next.config.js
@@ -1,11 +1,12 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   basePath: '/Next-js-better-you-app',
-  assetPrefix: '/Next-js-better-you-app',
+  assetPrefix: '/Next-js-better-you-app/',
   images: {
     loader: 'akamai',
     path: '',
   },
+  output: 'export',  // Add this line
 }
 
 module.exports = nextConfig
